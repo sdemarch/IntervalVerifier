069be0aa11d56b80fae2c39955fcd63470a1e769
Stefano Demarchi <stefano_demarchi@outlook.com>
2025-02-10 10:45:38 +0100

    Add VNNLIB example and simple parser

--- parser/vnnlib.py
+++ parser/vnnlib.py
@@ -1,2 +1,22 @@
+"""
+This module reads a VNNLIB file containing a robustness specification
+"""
+
+
 def read_vnnlib(filename: str) -> tuple[list[float], list[float], int]:
-    pass
+    lbs = []
+    ubs = []
+    label = filename.split('_')[-1].replace('.vnnlib', '')
+
+    with open(filename, 'r') as vnnlib_file:
+        # Input condition
+        for line in vnnlib_file:
+            if '>=' in line:
+                lbs.append(float(line.split()[-1].replace('))', '')))
+            elif '<=' in line:
+                ubs.append(float(line.split()[-1].replace('))', '')))
+            elif 'or' in line:
+                # We reached the output condition
+                break
+
+    return lbs, ubs, int(label)
