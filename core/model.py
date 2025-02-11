e4777b3ebfd194c8cad1c1abf23f541e05e8b309
aims-lab <stefano.demarchi@edu.unige.it>
2025-02-11 14:53:05 +0100

    Working on the robustness check

--- core/model.py
+++ core/model.py
@@ -23,13 +23,34 @@ class IntervalModel:
 
         self.layer = self.parse_layer()
 
-    @staticmethod
-    def check_robust(classifier_lbs: list, classifier_ubs: list, label: int) -> bool:
+    def check_robust(self, lbs: list, ubs: list, label: int) -> bool:
         """Procedure to check whether the robustness specification holds"""
-        correct = classifier_lbs[label]
-        classifier_ubs.pop(label)
+        correct = lbs[label]
+        others = list(ubs)
+        others.pop(label)
 
-        return ops.max_upper(classifier_ubs) < correct[0].inf
+        # Naive check - very unlikely
+        if ops.max_upper(others) < correct[0].inf:
+            return True
+        else:
+            n_disjunctions = len(others)
+
+            # Create the matrices of the disjunctions
+            matrix = ops.create_disjunction_matrix(len(lbs), label)
+            bounds = {
+                'lower': lbs,
+                'upper': ubs
+            }
+
+            # For each disjunction in the output property, check none is satisfied by output_bounds.
+            # If one disjunction is satisfied, then it represents a potential counter-example.
+            for i in range(n_disjunctions):
+                result = ops.check_unsafe(bounds, [matrix[i]], self.epsilon)
+
+                if result:
+                    return False
+
+            return True
 
     def parse_layer(self) -> LinearIntervalLayer:
         """Procedure to read the first layer of a ONNX network"""
@@ -58,4 +79,4 @@ class IntervalModel:
         out_lbs, out_ubs = self.propagate(in_lbs, in_ubs)
 
         # 4: Check output
-        return IntervalModel.check_robust(out_lbs, out_ubs, label)
+        return self.check_robust(out_lbs, out_ubs, label)
--- core/ops.py
+++ core/ops.py
@@ -1,8 +1,15 @@
 """
 This module defines common operations involving intervals
 """
+from enum import Enum
 
 from interval import interval
+
+
+class PropertySatisfied(Enum):
+    No = 0
+    Yes = 1
+    Maybe = 2
 
 
 def interval_from_value(value: float, epsilon: float):
@@ -77,3 +84,90 @@ def matmul(a: list, b: list):
             result[i][j] = s
 
     return result
+
+
+def create_disjunction_matrix(n_outs: int, label: int):
+    """Procedure to create the matrix of the output property"""
+    matrix = []
+    c = 0
+    for i in range(n_outs):
+        if i != label:
+            matrix.append([interval[0.0] for _ in range(n_outs)])
+            matrix[c][label] = interval[1.0]
+            matrix[c][i] = interval[-1.0]
+            c += 1
+
+    return matrix
+
+
+def compute_max(weights: list, bounds: dict):
+    """Procedure to compute the max value of the bounds through a linear transformation"""
+    # The result is a list with one element and I return the element directly
+    return add(matmul(get_positive(weights), bounds['lower']), matmul(get_negative(weights), bounds['upper']))[0]
+
+
+def compute_min(weights: list, bounds: dict):
+    """Procedure to compute the min value of the bounds through a linear transformation"""
+    # The result is a list with one element and I return the element directly
+    return add(matmul(get_positive(weights), bounds['upper']), matmul(get_negative(weights), bounds['lower']))[0]
+
+
+def check_unsafe(bounds: dict, matrix: list, epsilon: float):
+    """Procedure to check whether the output bounds are unsafe"""
+
+    possible_counter_example = False
+
+    disj_res = check_satisfied(bounds, matrix, epsilon)
+
+    if disj_res == PropertySatisfied.Yes:
+        # We are 100% sure there is a counter-example.
+        # It can be any point from the input space.
+        # Return anything from the input bounds
+        # input_bounds = nn_bounds.numeric_pre_bounds[nn.get_first_node().identifier]
+        return True  # , list(input_bounds.get_lower())
+    elif disj_res == PropertySatisfied.Maybe:
+        # We are not 100% sure there is a counter-example.
+        # Call an LP solver when we need a counter-example
+        possible_counter_example = True
+        print('Maybe unsafe')
+    else:  # disj_res == PropertySatisfied.No
+        # nothing to be done. Maybe other disjuncts will be satisfied
+        pass
+
+    # At least for one disjunct there is a possibility of a counter-example.
+    # Do a more powerful check with an LP solver
+    if possible_counter_example:
+        return False  # intersect_abstract_milp(star, nn, nn_bounds, prop)
+
+        # Every disjunction is definitely not satisfied.
+        # So we return False.
+    return False  # , []
+
+
+def check_satisfied(bounds: dict, matrix: list, epsilon: float):
+    """
+    Checks if the bounds satisfy the conjunction of constraints given by
+
+        matrix * x <= 0
+
+    Returns
+    -------
+    Yes if definitely satisfied
+    No if definitely not satisfied
+    Maybe when unsure
+    """
+
+    max_value = compute_max(matrix, bounds)
+    min_value = compute_min(matrix, bounds)
+
+    if min_value[0].inf > epsilon:
+        # the constraint j is definitely not satisfied, as it should be <= 0
+        return PropertySatisfied.No
+    elif max_value[0].sup > epsilon:
+        # the constraint j might not be satisfied, but we are not sure
+        return PropertySatisfied.Maybe
+    else:
+        # if we reached here, means that all max values were below 0
+        # so we now for sure that the property was satisfied
+        # and there is a counter-example (any point from the input bounds)
+        return PropertySatisfied.Yes
