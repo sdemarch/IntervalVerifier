8c7a38db364d1145c5e60f0a629965a2a28a2156
aims-lab <stefano.demarchi@edu.unige.it>
2025-03-10 10:36:24 +0100

    Fix interval unsafe check

--- core/ops.py
+++ core/ops.py
@@ -12,87 +12,62 @@ def interval_from_value(value: float, epsilon: float):
 
 def get_positive(a: list):
     """Procedure to extract the positive part of a matrix"""
-    if not isinstance(a[0], list):
-        aa = [a]
-    else:
-        aa = a
 
     result = [[0 for _ in range(len(a[0]))] for _ in range(len(a))]
 
-    for i in range(len(aa)):
-        for j in range(len(aa[i])):
-            if aa[i][j][0].sup > 0 > aa[i][j][0].inf:
-                result[i][j] = interval[0, aa[i][j][0].sup]
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            if a[i][j][0].sup > 0 > a[i][j][0].inf:
+                result[i][j] = interval[0, a[i][j][0].sup]
             else:
-                result[i][j] = aa[i][j] if aa[i][j][0].inf > 0 else interval[0.0, 0.0]
+                result[i][j] = a[i][j] if a[i][j][0].inf > 0 else interval[0.0, 0.0]
 
     return result
 
 
 def get_negative(a: list):
     """Procedure to extract the negative part of a matrix"""
-    if not isinstance(a[0], list):
-        aa = [a]
-    else:
-        aa = a
 
     result = [[0 for _ in range(len(a[0]))] for _ in range(len(a))]
 
-    for i in range(len(aa)):
-        for j in range(len(aa[i])):
-            if aa[i][j][0].sup > 0 > aa[i][j][0].inf:
-                result[i][j] = interval[aa[i][j][0].inf, 0]
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            if a[i][j][0].sup > 0 > a[i][j][0].inf:
+                result[i][j] = interval[a[i][j][0].inf, 0]
             else:
-                result[i][j] = aa[i][j] if aa[i][j][0].sup < 0 else interval[0.0, 0.0]
+                result[i][j] = a[i][j] if a[i][j][0].sup < 0 else interval[0.0, 0.0]
 
     return result
 
 
 def add(a: list, b: list, *args):
-    """Procedure to sum lists of intervals"""
+    """Procedure to sum column vectors of intervals"""
     result = []
 
-    if len(a) > 1:
-        for i in range(len(a)):
-            result.append(a[i][0] + b[i][0])
-            for x in args:
-                result[i] += x[i][0]
-
-    else:
+    if len(a) == 1:
         for i in range(len(a[0])):
             result.append(a[0][i] + b[0][i])
             for x in args:
                 result[i] += x[0][i]
 
-    return result
+        return [result]
 
+    else:
+        for i in range(len(a)):
+            result.append([a[i][0] + b[i][0]])
+            for x in args:
+                result[i][0] += x[i][0]
 
-def matmul_left(a: list, b: list):
-    """Procedure to multiply a matrix and a vector"""
-
-    assert len(a[0]) == len(b)
-
-    n = len(a)
-    m = len(b)
-    q = len(b[0])
-
-    result = [[0 for _ in range(q)] for _ in range(n)]
-
-    for i in range(n):
-        for j in range(q):
-            s = 0
-            for k in range(m):
-                s += a[i][k] * b[k]
-            result[i][j] = s
-
-    return result
+        return result
 
 
 def matmul(a: list, b: list):
-    """Procedure to multiply two matrices"""
+    """Procedure to multiply two matrices of intervals"""
 
     assert len(a[0]) == len(b)
 
+    # a = n x m, b = m x q
+    # -> result = n x q
     n = len(a)
     m = len(b)
     q = len(b[0])
@@ -130,13 +105,13 @@ def create_disjunction_matrix(n_outs: int, label: int):
 def compute_max(weights: list, bounds: dict):
     """Procedure to compute the max value of the bounds through a linear transformation"""
     # The result is a list with one element and I return the element directly
-    return add(matmul_left(get_positive(weights), bounds['lower']), matmul_left(get_negative(weights), bounds['upper']))[0]
+    return add(matmul(get_positive(weights), bounds['lower']), matmul(get_negative(weights), bounds['upper']))[0]
 
 
 def compute_min(weights: list, bounds: dict):
     """Procedure to compute the min value of the bounds through a linear transformation"""
     # The result is a list with one element and I return the element directly
-    return add(matmul_left(get_positive(weights), bounds['upper']), matmul_left(get_negative(weights), bounds['lower']))[0]
+    return add(matmul(get_positive(weights), bounds['upper']), matmul(get_negative(weights), bounds['lower']))[0]
 
 
 def check_unsafe(bounds: dict, matrix: list, epsilon: float) -> bool:
@@ -151,6 +126,7 @@ def check_unsafe_symbolic(bounds: dict, matrix: list, lbs: list, ubs: list, epsi
     out_m = add(matmul(get_positive(matrix), bounds['matrix']), matmul(get_negative(matrix), bounds['matrix']))
     out_b = add(matmul(get_positive(matrix), bounds['offset']), matmul(get_negative(matrix), bounds['offset']))
 
-    min_value = add(matmul_left(get_positive([out_m]), lbs), matmul_left(get_negative([out_m]), ubs), [out_b])[0]
+    min_matrix = add(matmul(get_positive(out_m), lbs), matmul(get_negative(out_m), ubs), out_b)
+    min_value = min_matrix[0][0]  # min_matrix is 1 x 1
 
     return min_value[0].inf <= epsilon
